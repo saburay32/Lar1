05174470316ff623b98ac2ca802e72fba8904c4e
Radimich <Radimich25@yandex.ru>
2020-07-09 19:49:56 +0300

    09_07_2020

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserOurRegistation(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['email','username','password1','password2']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from .forms import UserOurRegistation
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method =="POST":
+        form = UserOurRegistation(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Пользователь {username} был успешно создан!')
+            return redirect('blog-home')
+    else:
+        form = UserOurRegistation()
     return render(request,'users/registration.html',{'form':form,'title': 'Регистрация пользователя'})
