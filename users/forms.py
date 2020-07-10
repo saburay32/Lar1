990a3944e196096c17b7b86d6488f5e8556e54bf
Radimich <Radimich25@yandex.ru>
2020-07-10 18:22:32 +0300

    2 com 10_07_2020

--- users/forms.py
+++ users/forms.py
@@ -7,4 +7,4 @@ class UserOurRegistation(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['email','username','password1','password2']
+        fields = ['username','password1','password2','email']
