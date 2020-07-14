f61bc81057ca260e1c0371cc073a8541c20a0c3e
Radimich <Radimich25@yandex.ru>
2020-07-14 19:49:39 +0300

    2t com 14_07_2020

--- Lar1/urls.py
+++ Lar1/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from users import views as userViews
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/',userViews.register,name='reg'),
+    path('user/',authViews.LoginView.as_view,name='auth'),
     path('', include('blog.urls')),
 ]
